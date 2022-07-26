59bd735c4632b3e8f8438e9c8b7d8831ff1eb25d
gleonsrz <gonzalo15_115@hotmail.com>
2022-07-25 19:53:49 -0500

    First commit

---
+++ Assets/Scripts/Brain.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brain : MonoBehaviour
+{
+    public Transform enemyPrefab;
+    public Transform spawnPoint;
+
+    public float timeBetweenWaves = 5f;
+    private float countdown = 2f;
+    private int waveNumber = 0;
+
+    void Update()
+    {
+        if (countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+
+        countdown -= Time.deltaTime;
+    }
+
+    IEnumerator SpawnWave()
+    {
+        for (int i = 0; i < waveNumber; i++)
+        {
+            spawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+        waveNumber++;
+    }
+
+    void spawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public float speed = 10f;
+    private Transform target;
+    private int waypointindex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = Waypoints.points[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if (waypointindex == Waypoints.points.Length)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+            return;
+        }
+
+        if (Vector3.Distance(transform.position, target.position) < 0.4)
+        {
+            target = getNextTarget();
+        }
+    }
+
+    Transform getNextTarget()
+    {
+        if (waypointindex < Waypoints.points.Length)
+            waypointindex += 1;
+
+        if(waypointindex >= Waypoints.points.Length)
+            return Waypoints.points[Waypoints.points.Length-1];
+        else
+            return Waypoints.points[waypointindex];
+    }
+}
